bbacf3f58670e86f4660a0c7ec259e80fb30e6a9
mjassim343 <mjassim343@gmail.com>
2020-07-10 19:58:35 +0300

    first larave commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,13 @@
+
+    @extends('layouts.app')
+
+
+    @section('title','About page')
+
+
+    @section('content')
+    <div class="jumbotron">
+    <h1 class="display-4">About Us</h1>
+    <p class="lead">This is Jassim From Kuwait.</p>
+    </div>
+    @endsection 
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,15 @@
+
+    @extends('layouts.app')
+
+
+    @section('title','Contact page')
+
+
+    @section('content')
+    <div class="jumbotron">
+    <h1 class="display-4">Contact Us</h1>
+    <p class="lead">Mail us : mjassim343@gmail.com</p>
+    <hr class="my-4">
+    <p>Don't hesitate to contact us.</p>
+    </div>
+    @endsection
